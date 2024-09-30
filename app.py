94e291c2bd3057735df8f84f340e02bec46ecfdb
cmon-devops <rishmarun18@gmail.com>
2024-09-30 14:17:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
 
+
 app = Flask(__name__)
 
 # Initialize an empty list to store tasks
 tasks = []
 
+
 @app.route('/')
 def index():
     # Render the index.html template with the current tasks
     return render_template('index.html', tasks=tasks)
+
 
 @app.route('/add', methods=['POST'])
 def add_task():
@@ -17,6 +20,7 @@ def add_task():
     if task:  # Ensure the task is not empty
         tasks.append(task)
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
